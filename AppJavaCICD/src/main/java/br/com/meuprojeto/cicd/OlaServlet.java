68d4035845de60a69ec8f0b3ae5b115dd8810968
Wagner Souza Git <wlfsouza@hotmail.com>
2025-11-21 21:07:56 -0300

    primeira versao

---
+++ AppJavaCICD/src/main/java/br/com/meuprojeto/cicd/OlaServlet.java
@@ -0,0 +1,27 @@
+package br.com.meuprojeto.cicd;
+
+ 
+
+import java.io.IOException;
+// IMPORTANTE: Tomcat 10 usa jakarta.*
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+// Essa anotação define o "endereço" do servlet no navegador
+@WebServlet("/ola")
+public class OlaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Configura a resposta para ser lida corretamente com acentos
+		response.setContentType("text/html;charset=UTF-8");
+		
+		// Escreve a resposta na tela
+		response.getWriter().append("<h1>Funcionou!</h1>");
+		response.getWriter().append("<p>O Tomcat 10 respondeu usando Jakarta EE.</p>");
+		response.getWriter().append("<a href='index.jsp'>Voltar</a>");
+	}
+}
---
+++ AppJavaCICD/src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Teste Tomcat 10</title>
+</head>
+<body>
+    <h1>Bem-vindo ao Sistema Web</h1>
+    <p>Se você ver esta página, o JSP funcionou.</p>
+    
+    <hr>
+    
+    <h3>Teste do Backend:</h3>
+    <a href="ola">Clique aqui para chamar o Servlet</a>
+</body>
+</html>
